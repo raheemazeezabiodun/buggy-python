95898affb4a93bddf41b07420bd2a0885fc0698c
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2019-07-03 11:25:47 +0100

    Include note for the bug fixes

--- snippets/io.py
+++ snippets/io.py
@@ -12,20 +12,45 @@ def read_file():
 
 
 def calculate_unpaid_loans(data):
+    """
+    Calculate unpaid loans
+
+    :params :data -> dictionary (JSON Object)
+    :return int
+    """
     loans = data.get("loans")
-    unpaid_loans = [loan["amount"] for loan in loans if loan["status"] == "unpaid"]
+    unpaid_loans = [loan.get("amount") for loan in loans if loan.get("status") == "unpaid"]
     return sum(unpaid_loans)
 
 
-def calculate_paid_loans(data):
+def get_paid_loans(data):
+    """
+    Get paid loans
+
+    :params: data -> dictionary (JSON Object)
+    :return list
+    """
     loans = data.get("loans")
-    paid_loans = [loan.get("amount") for loan in loans if loan.get("status") == "paid"]
-    return sum(paid_loans)
+    return [loan.get("amount") for loan in loans if loan.get("status") == "paid"]
+
+def calculate_paid_loans(data):
+    """
+    Calculate paid loans
+
+    :params :data -> dictionary (JSON Object)
+    :return int
+    """
+    return sum(get_paid_loans(data))
 
 
 def average_paid_loans(data):
-    loans = data.get("loans")
-    paid_loans = [loan.get("amount") for loan in loans if loan.get("status") == "paid"]
+    """
+    Calculate average paid loans
+
+    :params :data -> dictionary (JSON Object)
+    :return int
+    """
+    paid_loans = get_paid_loans(data)
     sum_paid_loans = sum(paid_loans)
     number_paid_loans = len(paid_loans)
     average = (sum_paid_loans / number_paid_loans)
